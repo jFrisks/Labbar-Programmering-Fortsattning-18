0cbfd63ccaac17a203594feac39e956c13fbc8b2
Fredddaan <33512977+Fredddaan@users.noreply.github.com>
2018-02-14 20:56:57 +0100

    Update Side.java

--- lab4/src/mountain/Side.java
+++ lab4/src/mountain/Side.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class Side {
     Point p1;
     Point p2;
+    Point m;
 
 
     public Side(Point p1, Point p2){
@@ -15,4 +16,21 @@ public class Side {
     public int hashCode(){
         return p1.hashCode() + p2.hashCode();
     }
+    
+    public boolean equals(Object x) {
+		if (x instanceof Side) {
+			Side s = (Side) x; 
+			if(p1 == null && p2 == null){
+				return m.equals(s.m);				
+			} else {
+				return (p1.equals(s.p1) && p2.equals(s.p2)) || (p1.equals(s.p2) && p2.equals(s.p1)); 
+			}
+		}
+		return false;
+	}
+    
+    
+    public Point getMPoint(){
+		return m;
+	}
 }
