d447662debfbf17949f5d7abb043b598c5c76821
Jonathan Frisk <jontesport@gmail.com>
2018-02-27 14:57:30 +0100

    Labb6

--- lab6/src/map/SimpleHashMap.java
+++ lab6/src/map/SimpleHashMap.java
@@ -5,15 +5,18 @@ public class SimpleHashMap<K, V> implements Map<K, V>{
     private float loadFactor;
     private int capacity;
     private int size;
+    private int tableSize;
 
     public SimpleHashMap() {
         size = 0;
+        tableSize = 0;
         loadFactor = .75f;
         capacity = 16;
         entries = (Entry<K, V>[]) new Entry[capacity];
     }
     public SimpleHashMap(int capacity) {
         size = 0;
+        tableSize = 0;
         this.capacity = capacity;
         loadFactor = .75f;
         entries = (Entry<K, V>[]) new Entry[capacity];
@@ -21,17 +24,23 @@ public class SimpleHashMap<K, V> implements Map<K, V>{
 
     @Override
     public V get(Object key) {
-        //find key
-            //if found
-                //return found.value
-            //if not
-                //return null
+        //TODO: typkonvertera till K
+        K keyK = (K) key;
+        //eller gör jobbiga sättet, vilket är dumt
+
+        Entry<K, V> entry = find(keyK);
+        if(entry != null) {
+            entry.getValue();
+        }else {
+            return null;
+        }
+
         return null;
     }
 
     @Override
     public boolean isEmpty() {
-        if(size <= 0) {
+        if(size == 0) {
             return true;
         }else {
             return false;
@@ -40,9 +49,25 @@ public class SimpleHashMap<K, V> implements Map<K, V>{
 
     @Override
     public V put(K key, V value) {
-        //Få hashcodeplats
-        //lägg in ny entry i sista entry (entry.next == null)
-        return null;
+        //kolla om den överstiger loadfactor -> rehash
+        if(loadFactor <= tableSize/capacity){
+            rehash();
+        }else {
+            Entry<K, V> newEntry = new Entry<K, V>(key, value);
+
+            int index = index(key);
+            //kolla om vektorplats tom eller ej
+            if(entries[index] == null) {
+                entries[index] = newEntry;  //lägga in
+                tableSize++;
+            }else {
+                //lägg in ny entry i sista entry (entry.next == null)
+                Entry<K, V> oldFirstEntry = entries[index];
+                entries[index] = newEntry;
+                entries[index].next = oldFirstEntry;
+            }
+        }
+        return value;
     }
 
     @Override
@@ -56,16 +81,56 @@ public class SimpleHashMap<K, V> implements Map<K, V>{
         return null;
     }
 
-    private Entry<K, V> find(K key) {
-        //få index
-        //Loopa igenom alla entries
-            //returnera entry om den hittar
-            //annars null
+    private void rehash() {
+        //dubblera tabellen (ändra capacity och ny tabell)
+        capacity = capacity*2;
+        Entry<K, V>[] newTable = (Entry<K, V>[]) new Entry[capacity];
+
+        //loopa igenom gamla tabellen
+        for(Entry entry: entries) {
+
+            Entry<K, V> currentEntry = entry;
+
+            //loop igenom alla länkade entries
+            while(entry != null) {
+                int newIndex = entry.key.hashCode() % capacity;     //new index for entry in new table
+                newTable[newIndex] = currentEntry;
+
+                currentEntry = currentEntry.next;
+            }
+
+        }
+
+        entries = newTable;
     }
 
-    private int index(K key) {
-        //fixa hashcoden
-        //returnera var den borde ha för index
+    /**
+     *
+     * FIND
+     *
+     * */
+    private Entry<K, V> find(K key) {
+        int index = index(key);
+
+        //basentryn
+        Entry<K, V> currentEntry = entries[index];
+
+        while(entries[index] != null){
+            //cond for find key - no risk for nullpointer
+            if(currentEntry.key.equals(key)){
+                return currentEntry;
+            }
+            currentEntry = currentEntry.next;
+        }
+        return null;
+    }
+
+    private int index(Object key) {
+        int index;
+
+        //fixa hashcoden returnera var den borde ha för index
+        index = Math.abs(key.hashCode() % capacity);
+        return index;
     }
 
     public String show(){
@@ -75,10 +140,12 @@ public class SimpleHashMap<K, V> implements Map<K, V>{
             Entry<K, V> currentEntry = entries[i];  //keeps track of current entry of all that collides
 
             //loopa igenom alla next
+            sb.append(i + "         ");
             while(entries[i] != null) {
-                sb.append(i + "         " + entries[i].key.toString() + "=" +  entries[i].value.toString() + "\n");
+                sb.append(entries[i].key.toString() + "=" +  entries[i].value.toString() + " ");
                 currentEntry = currentEntry.next;
             }
+            sb.append("\n");
         }
         return sb.toString();
     }
---
+++ lab6/src/test/SimpleHashMapTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleHashMapTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+    }
+
+    @Test
+    public void put() throws Exception {
+    }
+
+    @Test
+    public void remove() throws Exception {
+    }
+
+    @Test
+    public void show() throws Exception {
+    }
+
+    @Test
+    public void size() throws Exception {
+    }
+
+}
