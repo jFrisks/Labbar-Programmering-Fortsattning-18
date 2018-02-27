d857132851bf7ee7968f6cf76e296d644198ed38
Jonathan Frisk <jontesport@gmail.com>
2018-02-27 16:26:48 +0100

    Init files

---
+++ Sudoku/src/sudoku/Board.java
@@ -0,0 +1,11 @@
+package sudoku;
+
+import javafx.stage.Stage;
+
+
+public class Board {
+
+    public Board(Stage stage){
+
+    }
+}
--- Sudoku/src/sudoku/Main.java
+++ Sudoku/src/sudoku/Main.java
@@ -1,8 +1,14 @@
 package sudoku;
 
+import javafx.stage.Stage;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        SudokuApp app = new SudokuApp();
+        Stage primaryStage = new Stage();
+
+        app.start(primaryStage);
     }
 }
